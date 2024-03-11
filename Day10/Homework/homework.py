d320713c592f42968839ba34590fbe160204b224
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-03-11 14:10:59 +0400

    ez

---
+++ Day10/Homework/homework.py
@@ -0,0 +1,19 @@
+#Goa is the best
+for i in range(1, 31):
+    print(i,"GOA IS THE BEST!")
+
+#5-150
+for p in range(5, 151):
+    print(p,"GOA")
+
+#2-50, 4
+for c in range(2, 52, 4):
+    print(c, "GOA")
+
+num1 = 20
+num2 = 21
+
+print(num1 < num2)
+print(num1 > num2)
+print(num2 == num1)
+print(num2 != num1)
