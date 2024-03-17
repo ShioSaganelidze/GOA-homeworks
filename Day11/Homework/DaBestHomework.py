f2502460e6497469d01996deda37480e5a1d8421
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-03-17 11:24:50 +0400

    e

---
+++ Day11/Homework/DaBestHomework.py
@@ -0,0 +1,14 @@
+# while True:
+#     print("Goa best")
+#________________________
+# while 5 > 3:
+#     print("Hello")
+#________________________
+# for i in range(30):
+#     print("Shio")
+#________________________
+word = "Easy"
+reversed_word = " "
+for i in word:
+    reversed_word = i + reversed_word
+    print(reversed_word)
