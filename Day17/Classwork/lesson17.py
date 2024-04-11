1ddbfef78c0e452ecc1cbfa6d39003527ad33105
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-04-11 18:44:13 +0400

    Forgot to upload again

---
+++ Day17/Classwork/lesson17.py
@@ -0,0 +1,1 @@
+product = ["milk", "bread", "oil"]
---
+++ Day17/Homework/Homework17.py
@@ -0,0 +1,5 @@
+Products = ["Apple", 7.1, "Oil", 2.8, "Bread", 8, "Water", 1, 9, "Salt"]
+Products[1] = "Soil"
+Products[2] = "Sourdough"
+Products[0] = "Orange"
+print(Products)
