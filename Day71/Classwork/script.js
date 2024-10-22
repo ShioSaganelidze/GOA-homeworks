255c723c1325ef113d0c6ca34e9ef98a8c4bb3d3
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-10-22 20:48:17 +0400

    First commit

---
+++ Day71/Classwork/script.js
@@ -0,0 +1,46 @@
+let name = prompt('Think of any number: ')
+alert('Reading mind...')
+alert('Processing brain cells...')
+alert('Guessing the number...')
+alert(`The number you thought of was ${name}`)
+
+// let x = Number(prompt('Enter x: '))
+// let y = Number(prompt('Enter y: '))
+// alert(x + y)
+
+// console.log(typeof 10);
+// console.log(typeof 10.5);
+// console.log(parseFloat('10.5'));
+
+// console.log(10 + 10.5)
+
+
+// let z = '10.5'
+// let a = 12
+// let updZ = parseFloat(x)
+// console.log(updX + y);
+
+// let x = '123abc'
+// // console.log(Number(x))
+// console.log(parseInt(x));
+
+// let age = Number(prompt('Enter your age: '))
+
+// if (age <= 12) {
+//     console.log("Child");
+// }
+
+// else if (age <= 19) {
+//     console.log("Teenager");
+// }
+
+// else if (age <= 64) {
+//     console.log("Adult");
+// }
+
+// else {
+//     console.log("Senior");
+// }
+
+// && = and - py
+// || = or - py
---
+++ Day71/Homework/script.js
@@ -0,0 +1,51 @@
+const pass = prompt()
+if (pass === 'Hello123') {
+    alert('Login successful')
+}
+else {
+    alert('Login failed. Please try again.');    
+}
+
+alert('Next task:')
+
+const traffic = prompt()
+if (traffic === 'Red') {
+    alert('Stop!')
+}
+else if (traffic === 'Yellow') {
+    alert('Prepare to stop.')
+}
+
+else if (traffic === 'Green') {
+    alert('Go!')
+}
+
+alert('Next task:')
+
+const age = prompt()
+if (age >= 18) {
+    alert('You can vote!')
+}
+
+else if (age < 18) {
+    alert('You cannot vote yet.')
+}
+
+alert('Next task:')
+
+const month = prompt
+if (month === 'December') {
+    alert('Happy Holidays')
+}
+
+else if (month === 'January') {
+    alert('Happy new Year!')
+}
+
+else if (month === 'February') {
+    alert(`Happy Valentine's Day!`)
+}
+
+else {
+    alert('Have a great month!')
+}
