b8c05ea96255450f84f8d9eae06980d7e70f5d8e
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-10-22 20:59:17 +0400

    First commit

---
+++ Day69/Homework/script.js
@@ -0,0 +1,8 @@
+const name = 'Shio'
+const surname = 'Saganelidze'
+const game = 'Football'
+const water = 'water'
+
+console.log(`Hello I am ${name} ${surname}`)
+console.log(`I like ${game}`)
+console.log(`I drink ${water}`)
