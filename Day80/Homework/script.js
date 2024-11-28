63b151734d1ef0d5ac63227b18807e217203a125
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-11-28 23:17:56 +0400

    First commit

---
+++ Day80/Homework/script.js
@@ -0,0 +1,17 @@
+let number = prompt('Enter a number: ')
+
+if (number % 3 == 0 && number % 5 == 0) {
+    alert('FizzBuz')
+}
+
+else if (number % 3 == 0) {
+    alert('Fizz')
+} 
+
+else if (number % 5 == 0) {
+    alert('Buzz')
+}
+
+else {
+    alert('Ver gavakete challenge 1')
+}
