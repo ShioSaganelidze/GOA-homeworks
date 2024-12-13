25bc626fecf076b280f6c45952be81ebdfc88d07
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-12-13 22:29:09 +0400

    First commit

---
+++ Day84/Classwork/script.js
@@ -0,0 +1,44 @@
+// for i in range(10):
+//     print(i)
+
+// for(let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// let arr1 = [10, 23, 43, 32, 11];
+// let res = 0;
+
+// for(let i = 0; i < arr1.length; i++) {
+//     res += arr1[i];
+// }
+// console.log(res);
+
+//______________________________________________
+
+// let sum = 0;
+
+// for(let i = 1; i <= 100; i++) {
+//     console.log(sum += i)
+// }
+
+// let num = Number(prompt('Enter a number: '))
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`)
+// }
+
+// let nums = [3, 6, 9, 13, 15, 18, 21, 24]
+
+// let countEven = 0;
+// let countOdd = 0;
+
+// for(let i = 0; i < nums.length; i++) {
+//     if(nums[i] % 2 === 0) {
+//         countEven++
+//     } else {
+//         countOdd++
+//     }
+// }
+
+// console.log(countEven)
+// console.log(countOdd);
---
+++ Day84/Homework/script.js
@@ -0,0 +1,41 @@
+// Task 1:
+
+// let sum = 0
+
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 3 === 0) {
+//         sum += i
+//     }
+// }
+
+// console.log(sum)
+
+// Task 2:
+
+// let isPrime = Number(prompt('Enter any number: '))
+// let isPrimeStatus = true;
+
+// for(let i = 2; i < isPrime; i++) {
+//     if(isPrime % i === 0) {
+//         isPrimeStatus = false;
+//         break;
+//     }
+// }
+
+// console.log(isPrime > 1 && isPrimeStatus ? 'Prime' : 'Not Prime')
+
+
+// Task 3:
+
+// let list = []
+// let num = Number(prompt(`Enter any number: `))
+
+// for(let i = 1; i <= num; i++) {
+//     if (num % i === 0) {
+//         list.push(i)
+//     }
+// }
+
+// console.log(list)
+
+// Task 4:
