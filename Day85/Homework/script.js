9044d33a77d0ccebd539f70100613fca4aa83f46
ShioSaganelidze <saganelidze.shio1980@gmail.com>
2024-12-13 22:28:40 +0400

    First commit

---
+++ Day85/Homework/script.js
@@ -0,0 +1,46 @@
+// Task 1:
+
+// let vowels = ['a', 'e', 'i', 'o', 'u']
+// let count = 0
+// let word = 'Hello world'
+
+// for (let i = 0; i <= word.length; i++) {
+//     if (vowels.includes[word]) {
+//         count += 1
+//     }
+// }
+
+// console.log(count)
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//I don't know how to fix this.
+
+// Task 2:
+
+// let sum = 0
+
+// for(let i = 0; i <= 100; i++) {
+//     sum += i
+// }
+
+// console.log(sum)
+
+// Task 3:
+
+// let num = prompt('Enter any number: ')
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(`${num} x ${i} = ${num * i}`)
+// }
+
+// Task 4:
+
+let word = 'javascript is fun'
+let letter = 'a'
+let count = 0
+
+for(let i = 0; i <= word.length; i++) {
+    if (word.includes[letter]) {
+        count += 1
+    }
+}
